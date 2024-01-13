909f6bbd73f93814c859958536339a8bb34adc31
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-13 22:23:11 +0000

    add login page path

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,13 +1,17 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import HomePage from './components/Home/HomePage';
 import MainLayout from './components/layouts/MainLayout';
+import LoginPage from './components/auth/LoginPage';
 
 const router = createBrowserRouter([
     {
         path: '/',
         id: 'root',
         element: <MainLayout />,
-        children: [{ index: true, element: <HomePage /> }],
+        children: [
+            { index: true, element: <HomePage /> },
+            { path: 'auth/login', element: <LoginPage /> },
+        ],
     },
 ]);
 
