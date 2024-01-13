05ae4e8e7fc7310b9e3da61f28893ed53aad932c
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-13 20:22:27 +0000

    create (HomePage) add hero img and text

--- app/src/components/Home/HomePage.jsx
+++ app/src/components/Home/HomePage.jsx
@@ -1,5 +1,28 @@
+import { Image, Text } from '@mantine/core';
+
 const HomePage = () => {
-    return <h1>Home</h1>;
+    return (
+        <>
+            <Image
+                fit='cover'
+                h='100vh'
+                style={{ width: '100%' }}
+                src='src/assets/images/pexels-malte-luk-1669754.jpg'
+            ></Image>
+            <Text
+                fw={900}
+                pos='absolute'
+                style={{
+                    top: '50%',
+                    left: '50%',
+                    fontSize: '80px',
+                    transform: 'translate(-50%, -50%)',
+                }}
+            >
+                Welcome to contractors jobs
+            </Text>
+        </>
+    );
 };
 
 export default HomePage;
