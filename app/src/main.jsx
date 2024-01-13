2155b8c8fdef8cf2cb9bcfb8080fc4bf2d22f04c
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-13 16:57:55 +0000

    add mantine theme and set default color sheme to light

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 
+import App from './App.jsx';
+import theme from './theme.js';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <MantineProvider>
+    <MantineProvider theme={theme} defaultColorScheme='light'>
         <React.StrictMode>
             <App />
         </React.StrictMode>
