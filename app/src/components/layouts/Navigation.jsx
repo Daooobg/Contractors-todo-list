0d777083348dc6e0373a2f90b9e0e944a796257d
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-13 20:24:15 +0000

    make navigation absolute

--- app/src/components/layouts/Navigation.jsx
+++ app/src/components/layouts/Navigation.jsx
@@ -17,16 +17,25 @@ const Navigation = () => {
     };
 
     return (
-        <Flex justify='space-between' gap='md' my='md' mx='xl' align='center'>
-            <Code fz='xl'>Contractors Jobs</Code>
-            <Group>
-                <Button>Login</Button>
-                <Button>Register</Button>
-            </Group>
-            <Button variant='link' onClick={toggleColorScheme}>
-                {computedColorScheme === 'light' ? <FaMoon /> : <FaSun />}
-            </Button>
-        </Flex>
+        <Group pos='absolute' style={{ zIndex: 10, width: '100%' }}>
+            <Flex
+                style={{ width: '100%' }}
+                justify='space-between'
+                gap='md'
+                my='md'
+                mx='xl'
+                align='center'
+            >
+                <Code fz='xl'>Contractors Jobs</Code>
+                <Group>
+                    <Button>Login</Button>
+                    <Button>Register</Button>
+                </Group>
+                <Button variant='link' onClick={toggleColorScheme}>
+                    {computedColorScheme === 'light' ? <FaMoon /> : <FaSun />}
+                </Button>
+            </Flex>
+        </Group>
     );
 };
 
