2db608f28e097b0c7bf2247b3c0e2583b66e00fc
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-13 16:56:46 +0000

    create (Mantine) theme

---
+++ app/src/theme.js
@@ -0,0 +1,8 @@
+import { createTheme } from '@mantine/core';
+
+const theme = createTheme({
+    fontFamily: 'Roboto, sans-serif',
+    fontFamilyMonospace: 'Monaco, Courier, monospace'
+});
+
+export default theme;
