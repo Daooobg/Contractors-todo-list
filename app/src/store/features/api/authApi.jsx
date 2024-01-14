8f730b1c1063042834ddb5171cd34930b783ff89
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-14 22:48:40 +0000

    init authApi and create register user mutation

---
+++ app/src/store/features/api/authApi.jsx
@@ -0,0 +1,29 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const authApi = createApi({
+    reducerPath: 'authApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: import.meta.env.VITE_BASE_URL,
+        prepareHeaders: async (headers) => {
+            headers.set('Content-Type', 'application/json');
+            return headers;
+        },
+    }),
+    endpoints(builder) {
+        return {
+            registerUser: builder.mutation({
+                query: (args) => {
+                    console.log(args)
+                    return {
+                        url: '/users/register',
+                        method: 'POST',
+                        body: JSON.stringify(args),
+                    };
+                },
+            }),
+        };
+    },
+});
+
+export const { useRegisterUserMutation } = authApi;
+export { authApi };
