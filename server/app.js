382af5872fe7598e58729b31bff489e321eb4c81
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-15 17:40:46 +0000

    use authMiddleware

--- server/app.js
+++ server/app.js
@@ -3,12 +3,15 @@ const cors = require('cors');
 
 const userRouter = require('./routes/userRouter');
 const errorMiddleware = require('./middlewares/errorMiddleware');
+const {authMiddleware} = require('./middlewares/authMiddleware');
 
 const app = express();
 app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(authMiddleware);
 
 app.use('/api/v1/users', userRouter);
 
