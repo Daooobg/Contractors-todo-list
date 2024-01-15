f896db3041628b6a62d463965cb23e402289896b
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-15 11:00:37 +0000

    use errorMiddleware

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const userRouter = require('./routes/userRouter');
+const errorMiddleware = require('./middlewares/errorMiddleware');
 
 const app = express();
 app.use(cors());
@@ -11,5 +12,6 @@ app.use(express.json());
 
 app.use('/api/v1/users', userRouter);
 
+app.use(errorMiddleware);
 
 module.exports = app;
