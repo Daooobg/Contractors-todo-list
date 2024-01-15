e7b5389f90bba89d6f2052e19002d9c3045a5722
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-15 11:24:11 +0000

    create login service

--- server/services/authService.js
+++ server/services/authService.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 
 const User = require('../models/userModel');
 const jwt = require('../lib/jsonwebtoken');
+const AppError = require('../utils/AppError');
 
 dotenv.config({ path: './config.env' });
 
@@ -34,5 +35,24 @@ const getUserByEmail = (email) => User.findOne({ email });
 exports.register = async (fullName, email, password, role) => {
     console.log(fullName, email, password, role);
     const user = await User.create({ fullName, email, password, role });
+    return createAndSendToken(user);
+};
+
+exports.login = async (email, password) => {
+    const user = await getUserByEmail(email);
+
+    if (!user) {
+        throw new AppError('Invalid Username or Password', 401, { email, password });
+    }
+    const isValidPassword = await user.validatePassword(password);
+
+    if (!isValidPassword) {
+        throw new AppError('Invalid Username or Password', 401, { email, password });
+    }
+
+    if (user.approved === false) {
+        throw new AppError('Your account is not approved yet!');
+    }
+
     return createAndSendToken(user);
 };
