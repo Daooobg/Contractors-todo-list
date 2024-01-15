d6aa37aef17bad0f048b73245600561cf77f068b
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-15 12:47:37 +0000

    create logout button and logout handler to dispatch removeUser action

--- app/src/components/layouts/Navigation.jsx
+++ app/src/components/layouts/Navigation.jsx
@@ -3,18 +3,27 @@ import {
     Code,
     Flex,
     Group,
+    Text,
     useComputedColorScheme,
     useMantineColorScheme,
 } from '@mantine/core';
 import { FaMoon, FaSun } from 'react-icons/fa';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { removeUser } from '../../store/features/slices/authSlice';
 
 const Navigation = () => {
+    const userData = useSelector((state) => state.auth.user);
+    const dispatch = useDispatch();
     const { setColorScheme } = useMantineColorScheme();
     const computedColorScheme = useComputedColorScheme('light');
 
     const toggleColorScheme = () => {
         setColorScheme(computedColorScheme === 'light' ? 'dark' : 'light');
+    };
+
+    const logoutHandler = () => {
+        dispatch(removeUser());
     };
 
     return (
@@ -29,12 +38,23 @@ const Navigation = () => {
             >
                 <Code fz='xl'>Contractors Jobs</Code>
                 <Group>
-                    <Button component={Link} to='/auth/login'>
-                        Login
-                    </Button>
-                    <Button component={Link} to='/auth/register'>
-                        Register
-                    </Button>
+                    {userData ? (
+                        <>
+                            <Text size='lg' tt='capitalize'>
+                                {userData.fullName}
+                            </Text>
+                            <Button onClick={logoutHandler}>Logout</Button>
+                        </>
+                    ) : (
+                        <>
+                            <Button component={Link} to='/auth/login'>
+                                Login
+                            </Button>
+                            <Button component={Link} to='/auth/register'>
+                                Register
+                            </Button>
+                        </>
+                    )}
                 </Group>
                 <Button variant='link' onClick={toggleColorScheme}>
                     {computedColorScheme === 'light' ? <FaMoon /> : <FaSun />}
