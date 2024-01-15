048dc6245a61d229752b7aba0ae8a654d7bcf5e6
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-15 18:47:44 +0000

    create (button) variant 'danger'

--- app/src/theme.js
+++ app/src/theme.js
@@ -1,8 +1,15 @@
-import { createTheme } from '@mantine/core';
+import { Button, createTheme } from '@mantine/core';
+
+import classes from './theme.module.css';
 
 const theme = createTheme({
     fontFamily: 'Roboto, sans-serif',
-    fontFamilyMonospace: 'Monaco, Courier, monospace'
+    fontFamilyMonospace: 'Monaco, Courier, monospace',
+    components: {
+        Button: Button.extend({
+            classNames: classes,
+        }),
+    },
 });
 
 export default theme;
