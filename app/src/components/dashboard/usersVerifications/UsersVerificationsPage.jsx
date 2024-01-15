9441a4c6341466c25c970fbf58251c0e90db415f
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-15 18:51:04 +0000

    use query to fetch all users for verification

--- app/src/components/dashboard/usersVerifications/UsersVerificationsPage.jsx
+++ app/src/components/dashboard/usersVerifications/UsersVerificationsPage.jsx
@@ -1,5 +1,22 @@
+import { Flex } from '@mantine/core';
+import { useGetAllUsersForVerificationQuery } from '../../../store/features/api/authApi';
+import UsersVerificationsCard from './UsersVerificationsCard';
+
 const UsersVerificationsPage = () => {
-    return <h1>UsersVerificationsPage</h1>;
+    const { data: usersForVerification, isSuccess } = useGetAllUsersForVerificationQuery();
+
+    return (
+        <>
+            <Flex gap='sm' wrap='wrap'>
+                {usersForVerification &&
+                    usersForVerification.length > 0 &&
+                    usersForVerification.map((user) => (
+                        <UsersVerificationsCard user={user} key={user._id} />
+                    ))}
+            </Flex>
+            {isSuccess && usersForVerification.length === 0 && <h1>There is not users</h1>}
+        </>
+    );
 };
 
 export default UsersVerificationsPage;
