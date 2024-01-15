741848ee2b8be183c352d8994da8cbbc2eee2f72
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-15 11:24:31 +0000

    create login controller

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -13,3 +13,15 @@ exports.register = catchAsync(async (req, res, next) => {
 
     res.status(200).json(token);
 });
+
+exports.login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return next(new AppError('Please provide Email and Password', 400, req.body));
+    }
+
+    const data = await authService.login(email, password);
+
+    res.status(200).json(data);
+});
