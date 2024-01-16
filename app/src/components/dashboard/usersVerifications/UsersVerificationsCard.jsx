020934714dc63bd91fe7f13073fc03ad2d32c61f
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-16 05:30:01 +0000

    create approveHandler

--- app/src/components/dashboard/usersVerifications/UsersVerificationsCard.jsx
+++ app/src/components/dashboard/usersVerifications/UsersVerificationsCard.jsx
@@ -1,15 +1,24 @@
 import { Button, Card, Group, Stack, Text } from '@mantine/core';
+import { useApproveUserMutation } from '../../../store/features/api/authApi';
 
 const UsersVerificationsCard = ({ user }) => {
+    const [approveUser] = useApproveUserMutation();
+
+    const approveHandler = () => {
+        approveUser(user._id);
+    };
+
     return (
         <Card m={30} shadow='sm' padding='lg' radius='md' withBorder>
             <Stack>
-                <Text size='lg' fw={500} tt='capitalize'>Full Name: {user.fullName}</Text>
+                <Text size='lg' fw={500} tt='capitalize'>
+                    Full Name: {user.fullName}
+                </Text>
                 <Text>Email: {user.email}</Text>
                 <Text>Role: {user.role}</Text>
                 <Text>Approved: {user.approved === false ? 'No' : 'Yes'}</Text>
                 <Group justify='space-between'>
-                    <Button>Approve</Button>
+                    <Button onClick={approveHandler}>Approve</Button>
                     <Button variant='danger'>Delete User</Button>
                 </Group>
             </Stack>
