9fb122ec3466e8bb6819230531429c2f02b8e5cf
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-16 05:17:33 +0000

    create approveUser path

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -6,5 +6,6 @@ const { authMiddleware } = require('../middlewares/authMiddleware');
 router.route('/register').post(authController.register);
 router.route('/login').post(authController.login);
 router.route('/usersVerifications').get(authController.usersVerifications);
+router.route('/approveUser').post(authController.updateUserApprove);
 
 module.exports = router;
