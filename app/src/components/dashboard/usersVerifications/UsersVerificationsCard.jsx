bc57ee32152363b6c4ca29a071c6035d617e9860
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-16 06:11:02 +0000

    create modal for confirm and use delete user mutation to remove the user

--- app/src/components/dashboard/usersVerifications/UsersVerificationsCard.jsx
+++ app/src/components/dashboard/usersVerifications/UsersVerificationsCard.jsx
@@ -1,15 +1,32 @@
-import { Button, Card, Group, Stack, Text } from '@mantine/core';
-import { useApproveUserMutation } from '../../../store/features/api/authApi';
+import { Button, Card, Flex, Group, Modal, Stack, Text } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
+import { useApproveUserMutation, useDeleteUserMutation } from '../../../store/features/api/authApi';
 
 const UsersVerificationsCard = ({ user }) => {
+    const [opened, { open, close }] = useDisclosure(false);
     const [approveUser] = useApproveUserMutation();
+    const [deleteUser] = useDeleteUserMutation();
 
     const approveHandler = () => {
         approveUser(user._id);
     };
 
+    const deleteUserHandler = () => {
+        deleteUser(user._id);
+    };
+
     return (
         <Card m={30} shadow='sm' padding='lg' radius='md' withBorder>
+            <Modal opened={opened} onClose={close} title='Delete user'>
+                <Text>Do you want to delete all user data for {user.fullName}?</Text>
+                <Flex mt={10} justify='space-around'>
+                    <Button variant='danger' onClick={deleteUserHandler}>
+                        Yes
+                    </Button>
+                    <Button onClick={close}>No</Button>
+                </Flex>
+            </Modal>
             <Stack>
                 <Text size='lg' fw={500} tt='capitalize'>
                     Full Name: {user.fullName}
@@ -19,7 +36,9 @@ const UsersVerificationsCard = ({ user }) => {
                 <Text>Approved: {user.approved === false ? 'No' : 'Yes'}</Text>
                 <Group justify='space-between'>
                     <Button onClick={approveHandler}>Approve</Button>
-                    <Button variant='danger'>Delete User</Button>
+                    <Button variant='danger' onClick={open}>
+                        Delete User
+                    </Button>
                 </Group>
             </Stack>
         </Card>
