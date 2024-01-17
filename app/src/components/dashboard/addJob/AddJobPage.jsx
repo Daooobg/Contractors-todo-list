f522b4f355a025231b6964b6a4df35e2214a3fcb
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-17 10:11:41 +0000

    create add jobs page

---
+++ app/src/components/dashboard/addJob/AddJobPage.jsx
@@ -0,0 +1,26 @@
+import { Card, Center, Stack, Text } from '@mantine/core';
+import { useState } from 'react';
+import AddAddress from './AddAddress';
+import AddJobProgress from './AddJobProgress';
+
+const AddJobPage = () => {
+    const [currentStep, setCurrentStep] = useState(1);
+
+    return (
+        <Center mt={80}>
+            <Stack>
+                <AddJobProgress step={currentStep} />
+                <Card w={300} shadow='sm' padding='lg' radius='md' withBorder>
+                    <Stack>
+                        <Text tt={'capitalize'} fz='xl' fw={900} mx='auto'>
+                            Create new job
+                        </Text>
+                        {currentStep === 1 && <AddAddress changeStep={setCurrentStep} />}
+                    </Stack>
+                </Card>
+            </Stack>
+        </Center>
+    );
+};
+
+export default AddJobPage;
