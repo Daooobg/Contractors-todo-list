228b24cf0333399c78ded4c9de53b8003197fdb6
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-20 06:07:45 +0000

    create add new contact service

--- server/services/jobsService.js
+++ server/services/jobsService.js
@@ -1,4 +1,7 @@
 const Address = require('../models/addressModel');
+const AppError = require('../utils/AppError');
+
+const findAddressById = (id) => Address.findById({ _id: id });
 
 exports.addAddress = async (data) => {
     const address = await Address.create(data);
@@ -10,4 +13,23 @@ exports.getAddressByPostcode = async (postcode) => {
     const addresses = await Address.find({ postcode: postcode }).select('-__v');
 
     return addresses;
+};
+
+exports.addNewContact = async (addressId, newContact) => {
+    try {
+        const address = await findAddressById(addressId);
+
+        if (!address) {
+            throw new AppError('Address not found');
+        }
+
+        address.contactDetails = address.contactDetails || [];
+
+        address.contactDetails.push(newContact);
+        const updatedAddress = await address.save();
+
+        return updatedAddress.contactDetails[updatedAddress.contactDetails.length - 1]._id;
+    } catch (error) {
+        throw new AppError(error);
+    }
 };
