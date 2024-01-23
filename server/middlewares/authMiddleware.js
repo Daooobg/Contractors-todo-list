fd6764c805f626e8da2ecf5b7e7cce3bf899865c
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-23 10:06:41 +0000

    on error throw new AppError with status code 401

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 const jwt = require('../lib/jsonwebtoken');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/AppError');
 
 dotenv.config({ path: './config.env' });
 
@@ -9,9 +10,16 @@ const SECRET = process.env.JWT_SECRET;
 exports.authMiddleware = catchAsync(async (req, res, next) => {
     if (req.header('Authorization')) {
         const token = req.header('Authorization');
-        if (token) {
-            const decodedToken = await jwt.verify(token, SECRET);
-            req.user = decodedToken;
+        try {
+            if (token) {
+                const decodedToken = await jwt.verify(token, SECRET);
+                req.user = decodedToken;
+            }
+            
+        } catch (error) {
+            if(error){
+                return next(new AppError(error, 401))
+            }
         }
     }
 
