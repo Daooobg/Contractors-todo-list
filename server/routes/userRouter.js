268cfe327ccb6342dd9b7ed42c37aa9305064959
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-23 12:14:54 +0000

    add new route

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
 const guards = require('../middlewares/guards');
 
 router.route('/register').post(authController.register);
@@ -12,5 +13,6 @@ router
     .route('/allApprovedUsers')
     .get(guards.restrictForAdmin, authController.getAllApprovedUsers)
     .post(guards.restrictForAdmin, authController.updateUser);
+router.route('/getAllContractors').get(userController.getAllContractors);
 
 module.exports = router;
