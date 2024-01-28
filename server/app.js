7cb8b5bcaff60a7a23009d2ac4d62e90cd00dcc4
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-28 18:23:13 +0000

    use image router

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 
 const userRouter = require('./routes/userRouter');
 const jobsRouter = require('./routes/jobsRouter');
+const imagesRouter = require('./routes/imagesRouter');
 
 const errorMiddleware = require('./middlewares/errorMiddleware');
 const { authMiddleware } = require('./middlewares/authMiddleware');
@@ -14,7 +15,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(authMiddleware);
-
+app.use('/api/v1/images', imagesRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/jobs', jobsRouter);
 
