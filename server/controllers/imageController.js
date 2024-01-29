0d100a36af3ece5f898beacb0141f4cbed32da6b
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-29 08:27:45 +0000

    send to imageService req.body and refactor the code

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -3,12 +3,12 @@ const AppError = require('../utils/AppError');
 const catchAsync = require('../utils/catchAsync');
 
 exports.addAllImages = catchAsync(async (req, res, next) => {
-    const issueId = req.params;
+    const jobId = req.params;
     const image = req.file;
-    if (!image || !issueId) {
+    if (!image || !jobId) {
         return next(new AppError('There is a problem please try again', 400));
     }
 
-    const imageUrl = await imageService.addAllImages(issueId.id, image);
+    const imageUrl = await imageService.addAllImages(jobId.id, image, req.body.issueId);
     res.status(200).json(imageUrl);
 });
