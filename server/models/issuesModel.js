53be686b7f89693fa3d72b18df4d90a4bb20ca46
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-30 07:47:41 +0000

    add more fields (status, progress, and paymentStatus

--- server/models/issuesModel.js
+++ server/models/issuesModel.js
@@ -51,6 +51,31 @@ const issuesSchema = new Schema({
         type: mongoose.Types.ObjectId,
         ref: 'User',
     },
+    // Job status
+    status: {
+        type: String,
+        lowercase: true,
+        enum: {
+            values: ['created', 'approved', 'finnish'],
+            message: 'Please select a correct status (Created, Approved, Finnish)',
+        },
+        default: 'created',
+    },
+    // Job progress
+    progress: {
+        type: String,
+        lowercase: true,
+        enum: {
+            values: ['created', 'booked', 'in progress', 'finish'],
+            message: 'Please select a correct status (Created, Booked, In progress, Finnish)',
+        },
+        default: 'created',
+    },
+    // Job payment status
+    paymentStatus: {
+        type: Boolean,
+        default: false,
+    },
 });
 
 const Issue = model('Issue', issuesSchema);
