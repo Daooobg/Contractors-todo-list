39c165fdb2d5710bc6220b33ee9d4a41299f5ab0
Ivaylo V Ivanov <88397502+Daooobg@users.noreply.github.com>
2024-01-30 15:34:19 +0000

    create new route

--- server/routes/jobsRouter.js
+++ server/routes/jobsRouter.js
@@ -8,6 +8,7 @@ router.route('/addNewContact/:id').post(jobsController.addNewContact);
 router.route('/contacts/:id').delete(jobsController.deleteContact).post(jobsController.editContact);
 router.route('/createJob').post(jobsController.createNewJob);
 router.route('/getByOwner').get(jobsController.getAllJobsByOwnerId);
+router.route('/getByContractor').get(jobsController.getAllJobsByContractorId);
 router.route('/jobs/:id').get(jobsController.getJobById);
 
 module.exports = router;
